478bde64d879b2ebe22f218aba891f5b4f204767
Avnish <avniniet@gmail.com>
2023-05-05 13:58:04 +0530

    Backedend Project - first commit

---
+++ onlinebookstore/bookish/src/test/java/in/project/bookish/BookishApplicationTests.java
@@ -0,0 +1,13 @@
+package in.project.bookish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
